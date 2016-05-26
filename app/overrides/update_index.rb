be8e5dcff7c4b281895a9a712ba28260ed78cbf9
Pedro <pedro@bydlowski.com>
2016-05-26 07:36:22 -0700

    Color changes

---
+++ app/overrides/update_index.rb
@@ -0,0 +1,4 @@
+Deface::Override.new( :virtual_path  => 'spree/shared/_sidebar',
+                      :name => 'Remove_sidebar_completely',
+                      :remove => '#sidebar',
+                      :disabled => false) 
