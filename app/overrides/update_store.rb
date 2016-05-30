d05df9bf9de2dd5c59f4f0f8777d2d30dd4767fc
Pedro <pedro@bydlowski.com>
2016-05-30 07:55:59 -0700

    Stock

--- app/overrides/update_store.rb
+++ app/overrides/update_store.rb
@@ -4,7 +4,34 @@ Deface::Override.new( :virtual_path  => 'spree/shared/_products',
                       :set_attributes => '[data-hook="products_list_item"]',
                       :attributes => {:class => 'col-md-6 col-sm-6 col-xs-6 product-list-item'})
 Deface::Override.new( :virtual_path  => 'spree/shared/_products',
+                      :name => 'Insert description',
+                      :insert_before => '.panel-footer',
+                      :text => '<div class="panel-footer text-center">
+                                  <span itemprop="description">
+                                    <span class="price selling lead"><%= product.description %></span>
+                                  </span>
+                                </div>',
+                      :disabled => false)
+Deface::Override.new( :virtual_path  => 'spree/shared/_products',
                       :name => 'Insert stock',
-                      :insert_after => 'erb[loud]:contains("small_image")',
-                      :text => '<br /><%= content_tag(:span, product.description, class: "info", itemprop: "description") %>',
-                      :disabled => false) 
+                      :replace => '[itemprop="offers"]',
+                      :text => '<span itemprop="description">
+                                  <span class="price selling lead">
+                                    <%= "Estoque: #{ 
+                                      if product.stock_items[0]["count_on_hand"] == 0 
+                                        "Esgotado :(" 
+                                      elsif product.stock_items[0]["count_on_hand"] == 1
+                                        "Um" 
+                                      elsif product.stock_items[0]["count_on_hand"] == 2
+                                        "Dois" 
+                                      elsif product.stock_items[0]["count_on_hand"] == 3
+                                        "Três"
+                                      elsif product.stock_items[0]["count_on_hand"] == 4
+                                        "Quatro"
+                                      else
+                                        "Muitos :)"
+                                      end
+                                    }" %>
+                                  </span>
+                                </span>',
+                      :disabled => false)
