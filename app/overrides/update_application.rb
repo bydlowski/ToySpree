02fce7cb74439aea2770d32e4da2727ed0bd97c9
Pedro <pedro@bydlowski.com>
2016-05-30 13:34:16 -0700

    About page

--- app/overrides/update_application.rb
+++ app/overrides/update_application.rb
@@ -4,7 +4,10 @@ Deface::Override.new( :virtual_path  => 'spree/layouts/spree_application',
                       :text => "
                       <div id='footer'>
                         <div class='footer-container'>
-                          <span><a href=''>bydlowski.com</a> &#169; 2016</span><span>About us</span>
+                          <span><%= link_to 'Início', spree.root_path %></span>
+                          <span><%= link_to 'Sobre a gente', main_app.sobre_path %></span>
+                          <span><%= link_to 'Contato', main_app.sobre_path %></span>
+                          <span class='right'><a href='http://bydlowski.com/'>bydlowski.com</a> &#169; 2016</span>
                         </div
                       </div>",
                       :disabled => false) 
